152b0e062f45f14faab585ac484be346431c6d7b
Kevin Oliveira <me@kevinoliveira.com.br>
2020-11-07 23:25:36 -0300

    fix(footer): Fix spotify text

--- src/js/components/spotify.js
+++ src/js/components/spotify.js
@@ -2,12 +2,13 @@ import SpotifyService from '../services/spotify';
 const spotifyService = new SpotifyService();
 
 const spotify = () => {
-  const $wrapper = document.querySelector('[data-spotify="wrapper"]');
+  const $wrapper = document.querySelector('[data-spotify="wrapper"] p');
   const $song = document.querySelector('[data-spotify="song"]');
   const $artist = document.querySelector('[data-spotify="artist"]');
 
   return spotifyService
     .then(res => {
+      console.log(res);
       $wrapper.classList.remove('hide');
       $song.innerHTML = res.data.item.name;
       $artist.innerHTML = res.data.item.artists[0].name;
