d0464f523849d1783d1df4e7386ce2940a88f1b9
Kevin Oliveira <me@kevinoliveira.com.br>
2023-05-26 00:42:16 -0300

    Feat: (pages) Add metatags

--- src/pages/head.tsx
+++ src/pages/head.tsx
@@ -3,8 +3,24 @@ export default function Head() {
     <>
       <title>Kevin Oliveira</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="" />
+      <meta name="description" content="Front-end Developer at Din Digital, previously at Agência Murilhas. Frontend & Design."/>
+
+      <meta property="og:url" content="https://kevinoliveira.com.br/"/>
+      <meta property="og:type" content="website"/>
+      <meta property="og:title" content="Kevin Oliveira"/>
+      <meta property="og:description" content="Front-end Developer at Din Digital, previously at Agência Murilhas. Frontend & Design."/>
+      <meta property="og:image" content="/img/code-in-the-dark.png"/>
+
+      <meta name="twitter:card" content="summary_large_image"/>
+      <meta property="twitter:url" content="https://kevinoliveira.com.br/"/>
+      <meta name="twitter:title" content="Kevin Oliveira"/>
+      <meta name="twitter:description" content="Front-end Developer at Din Digital, previously at Agência Murilhas. Frontend & Design."/>
+      <meta name="twitter:image" content="/img/code-in-the-dark.png"/>
       <link rel="icon" href="/favicon.ico" />
+      <meta
+        name="google-site-verification"
+        content="e8tKK8_XjvMNTjaAiBPkX3bu_03VuuQzNjcHFTMS8mI"
+      />
     </>
   )
 }
