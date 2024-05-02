02150305fa358fa1a55325c8cc0a0692c22603ab
Kevin Oliveira <me@kevinoliveira.com.br>
2024-05-02 13:33:43 -0300

    Fix footer link

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -14,7 +14,7 @@ export default function Footer() {
         <LinkExternal title="GitHub" href="https://github.com/kvnol" />
       </nav>
       <p className={styles.copy}>2023 &copy; Kevin Oliveira</p>
-      <p className={styles.copy}><a href="https://nubank.com.br/pagar/4tc3b/4e5TDadPmY"><small><Icon icon="eva:heart-fill" /> Buy me a coffee</small></a></p>
+      <p className={styles.copy}><a href="https://nubank.com.br/pagar/4tc3b/4e5TDadPmY" target="_blank" rel="noopener noreferrer"><small><Icon icon="eva:heart-fill" /> Buy me a coffee</small></a></p>
     </footer>
   )
 }
